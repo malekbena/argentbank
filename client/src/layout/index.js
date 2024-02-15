2a618570ae2e7bc226bf2bffbe8cc16a37ea49c9
malekbena <malekb.benali@gmail.com>
2024-02-15 13:44:25 +0100

    client: get user accounts if page reloaded

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Outlet } from "react-router-dom"
 import { useDispatch } from "react-redux";
-import { getProfile } from "../redux/userSlice";
+import { getProfile, getAccounts } from "../redux/userSlice";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { checkToken } from "../utils";
@@ -9,7 +9,10 @@ import { checkToken } from "../utils";
 const Layout = () => {
     const dispatch = useDispatch()
     useEffect(() => {
-        checkToken() && dispatch(getProfile(localStorage.getItem('token')))
+        if (checkToken()) {
+            dispatch(getProfile(localStorage.getItem('token')))
+            dispatch(getAccounts(localStorage.getItem('token')))
+        }
     }, [dispatch])
     return (
         <>
